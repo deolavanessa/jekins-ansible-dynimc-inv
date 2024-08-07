f9ff99f39f85993cf15900d7cab65c684bdf847d
deolavanessa <deolavanessa@yahoo.com>
2024-08-07 14:42:03 -0700

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,11 +1,11 @@
-provider "aws"{
-region = "ap-south-1"
+provide "aws"{
+region = "ca-central-1"
 }
 
 resource "aws_instance" "AWS_EC2INSTANCE"{
-ami = "ami-068e0f1a600cd311c"
+ami = "ami-0dcab815e6ee910aa"
 instance_type = "t2.micro"
-key_name = "devopsmssjuly"
+key_name = "devopsmssmay2024"
 security_groups = ["launch-wizard-2"]
 tags = {
 Name = "terraformserver"
